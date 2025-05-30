3f4cc36500b3459d2039cd8d01bee1d314f8a5de
KONGKONGTOP <70913559+KONGKONGTOP@users.noreply.github.com>
2025-05-30 16:50:38 +0900

    Update attendance.sql
    테이블 설계 했던것들 빼먹어서 올림.

--- smart_factory/day_01/attendance.sql
+++ smart_factory/day_01/attendance.sql
@@ -1,3 +1,27 @@
+-- Students 테이블
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    gender ENUM('M','F'),
+    enrollment_date DATE
+);
+
+-- Attendance 테이블
+CREATE TABLE Attendance (
+    attendance_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    date DATE,
+    status ENUM('출석','지각','결석','조퇴'),
+    FOREIGN KEY (student_id) REFERENCES Students(student_id)
+);
+
+-- Classes 테이블
+CREATE TABLE Classes (
+    class_id INT PRIMARY KEY AUTO_INCREMENT,
+    class_name VARCHAR(100),
+    class_time TIME
+);
+
 INSERT INTO students (name, gender, enrollment_date) values
 ('홍길동', 'm' , '2024-03-01'),
 ('김영희', 'f' , '2024-03-01'),
